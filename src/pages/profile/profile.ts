b64b8a04fd41903656e295547b908223eca465aa
vrsmarcos00 <vrr.marcos@gmail.com>
2021-11-29 17:13:25 -0300

    Handling 403 errors

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -31,7 +31,14 @@ export class ProfilePage {
           //buscar imagem
           this.getImageIfExist();
         },
-        error => {});
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+          else {
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
     }
   }
 
