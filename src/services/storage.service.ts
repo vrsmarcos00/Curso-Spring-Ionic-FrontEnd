f79e920a43843551a4d3aacf0a1afedaeaf9ba4a
vrsmarcos00 <vrr.marcos@gmail.com>
2021-12-01 18:40:11 -0300

    Creating Shopping Cart Page

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from "@angular/core";
 import { STORAGE_KEY } from "../config/storage_key.config";
+import { Cart } from "../models/cart";
 import { LocalUser } from "../models/local_user";
 
 @Injectable()
 export class StorageService {
 
   getLocalUser() : LocalUser {
-
     let usr = localStorage.getItem(STORAGE_KEY.localUser);
     if(usr == null) {
       return null;
@@ -14,18 +14,34 @@ export class StorageService {
     else {
       return JSON.parse(usr);
     }
-
   }
 
   setLocalUser(obj : LocalUser) {
-
     if(obj == null) {
       localStorage.removeItem(STORAGE_KEY.localUser);
     }
     else {
       localStorage.setItem(STORAGE_KEY.localUser, JSON.stringify(obj));
     }
+  }
 
+  getCart() : Cart {
+    let str = localStorage.getItem(STORAGE_KEY.cart);
+    if(str != null) {
+      return JSON.parse(str);
+    }
+    else {
+      return null;
+    }
+  }
+
+  setCart(obj : Cart) {
+    if(obj != null) {
+      localStorage.setItem(STORAGE_KEY.cart, JSON.stringify(obj));
+    }
+    else {
+      localStorage.removeItem(STORAGE_KEY.cart);
+    }
   }
 
 }
