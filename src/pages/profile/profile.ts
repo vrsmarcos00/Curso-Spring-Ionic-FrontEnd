83e4db44587ad41d7c8499320a74c87ea2cb2484
vrsmarcos00 <vrr.marcos@gmail.com>
2021-12-03 15:35:49 -0300

    Uploading the photo

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,18 +1,17 @@
-import { Component } from '@angular/core';
-import { Camera, CameraOptions } from '@ionic-native/camera';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { API_CONFIG } from '../../config/api.config';
-import { ClienteDTO } from '../../models/cliente.dto';
-import { ClienteService } from '../../services/domain/cliente.service';
-import { StorageService } from '../../services/storage.service';
+import { Component } from "@angular/core";
+import { Camera, CameraOptions } from "@ionic-native/camera";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { API_CONFIG } from "../../config/api.config";
+import { ClienteDTO } from "../../models/cliente.dto";
+import { ClienteService } from "../../services/domain/cliente.service";
+import { StorageService } from "../../services/storage.service";
 
 @IonicPage()
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
+  selector: "page-profile",
+  templateUrl: "profile.html",
 })
 export class ProfilePage {
-
   cliente: ClienteDTO;
   picture: string;
   cameraOn: boolean = false;
@@ -23,56 +22,75 @@ export class ProfilePage {
     public storage: StorageService,
     public clienteService: ClienteService,
     public camera: Camera
-    ) {
-  }
+  ) {}
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
-      this.clienteService.findByEmail(localUser.email)
-        .subscribe(response => {
+      this.clienteService.findByEmail(localUser.email).subscribe(
+        (response) => {
           this.cliente = response as ClienteDTO;
           //buscar imagem
           this.getImageIfExist();
         },
-        error => {
+        (error) => {
           if (error.status == 403) {
-            this.navCtrl.setRoot('HomePage');
+            this.navCtrl.setRoot("HomePage");
+          } else {
+            this.navCtrl.setRoot("HomePage");
           }
-          else {
-            this.navCtrl.setRoot('HomePage');
-          }
-        });
+        }
+      );
     }
   }
 
   getImageIfExist() {
-    this.clienteService.getImageFromBucket(this.cliente.id)
-      .subscribe(response => {
+    this.clienteService.getImageFromBucket(this.cliente.id).subscribe(
+      (response) => {
         this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
       },
-      error => {});
+      (error) => {}
+    );
   }
 
   getCameraPicture() {
-
     this.cameraOn = true;
 
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.PNG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
+      mediaType: this.camera.MediaType.PICTURE,
+    };
 
-    this.camera.getPicture(options).then((imageData) => {
-     // imageData is either a base64 encoded string or a file URI
-     // If it's base64 (DATA_URL):
-     this.picture = 'data:image/png;base64,' + imageData;
-     this.cameraOn = false;
-    }, (err) => {
-     // Handle error
-    });
+    this.camera.getPicture(options).then(
+      (imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64 (DATA_URL):
+        this.picture = "data:image/png;base64," + imageData;
+        this.cameraOn = false;
+      },
+      (err) => {
+        // Handle error
+      }
+    );
   }
 
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture).subscribe(
+      (response) => {
+        this.picture = null;
+        this.loadData();
+      },
+      (error) => {}
+    );
+  }
+
+  cancel() {
+    this.picture = null;
+  }
 }
