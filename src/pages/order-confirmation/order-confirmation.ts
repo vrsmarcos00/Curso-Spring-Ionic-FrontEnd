71dca6b4700ff2313f8c431923540e3d27213193
vrsmarcos00 <vrr.marcos@gmail.com>
2021-12-03 11:14:14 -0300

    Save Order

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { EnderecoDTO } from '../../models/endereco.dto';
 import { PedidoDTO } from '../../models/pedido.dto';
 import { CartService } from '../../services/domain/cart.service';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @IonicPage()
 @Component({
@@ -23,7 +24,8 @@ export class OrderConfirmationPage {
     public navCtrl: NavController,
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService:  ClienteService) {
+    public clienteService:  ClienteService,
+    public pedidoService: PedidoService) {
 
       this.pedido = this.navParams.get('pedido');
 
@@ -49,6 +51,23 @@ export class OrderConfirmationPage {
 
   total(){
     return this.cartService.total();
+  }
+
+  checkout() {
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+        console.log(response.headers.get('location'));
+        this.cartService.createOrClearCart();
+      },
+      error => {
+        if (error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+  }
+
+  backPage() {
+    this.navCtrl.setRoot('CartPage');
   }
 
 }
---
+++ src/services/domain/pedido.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { API_CONFIG } from "../../config/api.config";
+import { PedidoDTO } from "../../models/pedido.dto";
+
+@Injectable()
+export class PedidoService {
+  constructor(
+    public http: HttpClient
+  ){}
+
+  insert(obj: PedidoDTO) {
+    return this.http.post(
+      `${API_CONFIG.baseUrl}/pedidos`,
+      obj,
+      {
+        observe: 'response',
+        responseType: 'text' //Back-end retorna resposta com corpo vazio por ser um método post, se não colocar text a aplicação vai achar que é JSON e vai tentar dar um parse, e parse no vazio irá dar erro.
+      }
+    );
+  }
+
+}
